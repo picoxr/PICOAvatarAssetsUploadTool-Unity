c80bb9f776d65b9c39d90813167aa2869788f7d4
jinmingfei <jinmingfei@bytedance.com>
2024-06-06 11:06:19 +0800

    +Fix Path Error

--- Assets/PicoAvatarAssetPreview/quarantine.sh
+++ Assets/PicoAvatarAssetPreview/quarantine.sh
@@ -1,5 +1,9 @@
-xattr -r -d com.apple.quarantine Assets/PicoAvatar/Plugins/OSX/libs/libassimp.5.dylib
-xattr -r -d com.apple.quarantine Assets/PicoAvatar/Plugins/OSX/libs/libavatarloader.dylib
-xattr -r -d com.apple.quarantine Assets/PicoAvatar/Plugins/OSX/libs/libbytenn.dylib
-xattr -r -d com.apple.quarantine Assets/PicoAvatar/Plugins/OSX/libs/libeffect.dylib
-xattr -r -d com.apple.quarantine Assets/PicoAvatar/Plugins/OSX/libs/libsscronet.dylib
+pwd
+xattr -r -d com.apple.quarantine ../../CustomPackages/PICOUnityAvatarSDK-2.0.0/Editor/Plugins/OSX/libs/libassimp.5.dylib
+xattr -r -d com.apple.quarantine ../../CustomPackages/PICOUnityAvatarSDK-2.0.0/Editor/Plugins/OSX/libs/libavatarloader.dylib
+xattr -r -d com.apple.quarantine ../../CustomPackages/PICOUnityAvatarSDK-2.0.0/Editor/Plugins/OSX/libs/libbytenn.dylib
+xattr -r -d com.apple.quarantine ../../CustomPackages/PICOUnityAvatarSDK-2.0.0/Editor/Plugins/OSX/libs/libeffect.dylib
+xattr -r -d com.apple.quarantine ../../CustomPackages/PICOUnityAvatarSDK-2.0.0/Editor/Plugins/OSX/libs/libsscronet.dylib
+xattr -r -d com.apple.quarantine ../../CustomPackages/PICO Unity Integration SDK_250/SpatialAudio/Plugins/OSX/libPicoAmbisonicDecoder.dylib
+xattr -r -d com.apple.quarantine ../../CustomPackages/PICO Unity Integration SDK_250/SpatialAudio/Plugins/OSX/libPicoAudioRouter.dylib
+xattr -r -d com.apple.quarantine ../../CustomPackages/PICO Unity Integration SDK_250/SpatialAudio/Plugins/OSX/libPicoSpatializer.dylib
